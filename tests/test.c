3d9b675c85eb1ae7b5eab9f0233b8e2553eb99bc
maithaalhayyas <maithaalhayyas@gmail.com>
2021-06-17 10:53:09 +0400

    delete the tests that are not relevant

--- tests/test.c
+++ tests/test.c
@@ -5,23 +5,6 @@
 
 #include "../prod.h"
 
-static void Test_number_multiple_of_3_is_translated_to_Fizz(void **state)
-{
-	assert_string_equal("Fizz", fizzBuzz(6));
-	assert_string_equal("Fizz", fizzBuzz(3));
-	assert_string_equal("Fizz", fizzBuzz(9));
-	assert_string_equal("Fizz", fizzBuzz(12));
-}
-
-static void Test_number_5_is_translated_to_Buzz(void **state)
-{
-	assert_string_equal("Buzz", fizzBuzz(5));
-}
-
-static void Test_number_15_is_translated_to_FizzBuzz(void **state)
-{
-    assert_string_equal("FizzBuzz", fizzBuzz(15));
-}
 
 /* A test case that does nothing and succeeds. */
 static void canary_test(void **state) {
@@ -31,9 +14,7 @@ static void canary_test(void **state) {
 int main(void) {
     const struct CMUnitTest tests[] = {
             cmocka_unit_test(canary_test)
-            , cmocka_unit_test(Test_number_15_is_translated_to_FizzBuzz)
-            , cmocka_unit_test(Test_number_5_is_translated_to_Buzz)
-            , cmocka_unit_test(Test_number_multiple_of_3_is_translated_to_Fizz)
+
     };
     return cmocka_run_group_tests(tests, NULL, NULL);
 }
